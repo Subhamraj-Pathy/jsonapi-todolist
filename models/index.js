f1bfd86850319c49df5acf34b9aa3553218a41c2
Subhamraj Pathy <subhamrajpathy@gmail.com>
2019-01-07 00:18:29 +0000

    mLab URI Added

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 
 mongoose.set('debug' , true);
 
-mongoose.connect('mongodb://localhost/todoapi');
+// mongoose.connect('mongodb://localhost/todoapi');
+mongoose.connect('mongodb://Subham:Subham111@ds149984.mlab.com:49984/jsonapi-todolist');
 
 mongoose.Promise = Promise;
 
